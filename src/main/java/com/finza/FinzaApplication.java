d7cfb0a71b056ef2b5dff58b7f3b08c3bb816db5
unknown <igormatheussiquieri@gmail.com>
2026-08-26 21:59:03 -0300

    Initial commit

---
+++ src/main/java/com/finza/FinzaApplication.java
@@ -0,0 +1,13 @@
+package com.finza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinzaApplication.class, args);
+	}
+
+}
